76d23da99ee73fa21dbe3d0158e4b854a6b3e4f3
GnaixEuy <sgghcj@live.com>
2021-10-15 20:38:50 +0800

    Initial Commit

---
+++ CyWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CyWeather
+//
+//  Created by 苏粤翔 on 2021/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CyWeather/CyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyWeatherApp.swift
+//  CyWeather
+//
+//  Created by 苏粤翔 on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct CyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
